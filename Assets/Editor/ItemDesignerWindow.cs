42d499430c5d354536ea691fedca7ea52f69281c
MCBHammer <armanaznavour@yahoo.com>
2021-10-19 02:47:09 -0500

    Added Folders and Started

---
+++ Assets/Editor/ItemDesignerWindow.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class ItemDesignerWindow : EditorWindow
+{
+    [MenuItem("Window/Item Designer")]
+    static void OpenWindow()
+    {
+        ItemDesignerWindow window = (ItemDesignerWindow)GetWindow(typeof(ItemDesignerWindow));
+        window.minSize = new Vector2(600, 300);
+        //window.maxSize
+        window.Show();
+    }
+}
---
+++ Assets/Scripts/Types.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Types
+{
+    public enum ItemType
+    {
+        HAT,
+        ROBE,
+        SHOES,
+        WAND,
+        AMULET,
+        ATHAME,
+        RING,
+        DECK
+    }
+
+    public enum Schools
+    {
+        UNIVERSAL,
+        BALANCE,
+        DEATH,
+        FIRE,
+        ICE,
+        LIFE,
+        MYTH,
+        STORM,
+    }
+}
