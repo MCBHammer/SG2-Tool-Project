459a770011ef20927c8eafe174bf4d87a713f6ab
MCBHammer <armanaznavour@yahoo.com>
2021-10-19 21:58:09 -0500

    Added Added data stuff, removed test hat

--- Assets/Resources/ItemData/Scripts/ItemData.cs
+++ Assets/Resources/ItemData/Scripts/ItemData.cs
@@ -1,10 +1,39 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Types;
 
 [CreateAssetMenuAttribute(fileName = "New Item Data", menuName = "ItemData")]
 public class ItemData : BaseData
 {
-    int MaxHP;
-    int MaxMP;
+    public int maxHP;
+    public int maxMP;
+    public DamageBuff damageBuff;
+    public AccuracyBuff accuracyBuff;
+    public CritBuff critBuff;
+    public ResistanceBuff resistanceBuff;
+}
+
+public class DamageBuff : ItemData
+{
+    public Schools buffSchool;
+    public int buffValue;
+}
+
+public class AccuracyBuff : ItemData
+{
+    public Schools buffSchool;
+    public int buffValue;
+}
+
+public class CritBuff : ItemData
+{
+    public Schools buffSchool;
+    public int buffValue;
+}
+
+public class ResistanceBuff : ItemData
+{
+    public Schools buffSchool;
+    public int buffValue;
 }
